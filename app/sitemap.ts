5efe4e7c5d6cb9e7a60e62c18e787860afd13f16
Father1993 <enjoyhillol@gmail.com>
2024-10-11 13:08:13 +1000

    fix bugs with bouild

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,6 @@ import { MetadataRoute } from 'next'
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = 'https://spinej.ru'
 
-
   return [
     {
       url: baseUrl,
